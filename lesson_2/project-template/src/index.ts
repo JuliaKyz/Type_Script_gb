588bbdc5d5e332b4fecff8cd1bdc84972a06b4d0
JuliaKyz <Julie.abukhova@gmail.com>
2023-01-31 01:45:47 +0300

    added lesson 3

--- lesson_2/project-template/src/index.ts
+++ lesson_2/project-template/src/index.ts
@@ -1,10 +1,11 @@
-import { renderSearchFormBlock } from './search-form.js'
-import { renderSearchStubBlock } from './search-results.js'
-import { renderUserBlock } from './user.js'
-import { renderToast } from './lib.js'
-
-
-
+import { renderSearchFormBlock } from "./search-form.js";
+import {
+  renderSearchResultsBlock,
+  renderSearchStubBlock,
+} from "./search-results.js";
+import { getFavoritesAmount, renderUserBlock } from "./user.js";
+// import { renderToast } from "./lib.js";
+declare const window: Window;
 if (typeof window !== undefined && window && window.addEventListener) {
   window.addEventListener("DOMContentLoaded", () => {
     const today = new Date();
@@ -19,21 +20,23 @@ if (typeof window !== undefined && window && window.addEventListener) {
       today.getDate() + 3
     );
 
-    renderUserBlock("Anna", "/img/avatar.png", 3);
+    renderUserBlock("Anton Pryakhin", "/img/avatar.png", getFavoritesAmount());
     renderSearchFormBlock(dateIn, dateOut);
     renderSearchStubBlock();
-    renderToast(
-      {
-        text: "Это пример уведомления. Используйте его при необходимости",
-        type: "success",
-      },
-      {
-        name: "Понял",
-        handler: () => {
-          console.log("Уведомление закрыто");
-        },
-      }
-    );
+    // renderSearchResultsBlock();
+
+    // renderToast(
+    //   {
+    //     text: "Это пример уведомления. Используйте его при необходимости",
+    //     type: "success",
+    //   },
+    //   {
+    //     name: "Понял",
+    //     handler: () => {
+    //       console.log("Уведомление закрыто");
+    //     },
+    //   }
+    // );
   });
 } else {
   console.log("This is not browser!");
