121549fb7bf6bbdb5030c0b832398dd2bb057133
JuliaKyz <Julie.abukhova@gmail.com>
2023-01-31 14:38:21 +0300

    added lesson 5

--- lesson_2/project-template/src/lib.ts
+++ lesson_2/project-template/src/lib.ts
@@ -1,29 +1,30 @@
-export function renderBlock(elementId, html) {
-  const element = document.getElementById(elementId);
-  element.innerHTML = html;
-}
+import { renderSearchFormBlock } from "./search-form.js";
+import {
+  // renderSearchResultsBlock,
+  renderSearchStubBlock,
+} from "./search-results.js";
+import { getFavoritesAmount, renderUserBlock } from "./user.js";
+// import { renderToast } from "./lib.js";
+declare const window: Window;
+if (typeof window !== undefined && window && window.addEventListener) {
+  window.addEventListener("DOMContentLoaded", () => {
+    const today = new Date();
+    const dateIn = new Date(
+      today.getFullYear(),
+      today.getMonth(),
+      today.getDate() + 1
+    );
+    const dateOut = new Date(
+      today.getFullYear(),
+      today.getMonth(),
+      today.getDate() + 3
+    );
 
-export function renderToast(message, action) {
-  let messageText = "";
+    renderUserBlock("Anton Pryakhin", "/img/avatar.png", getFavoritesAmount());
+    renderSearchFormBlock(dateIn, dateOut);
+    renderSearchStubBlock();
 
-  if (message != null) {
-    messageText = `
-      <div id="info-block" class="info-block ${message.type}">
-        <p>${message.text}</p>
-        <button id="toast-main-action">${action?.name || "Закрыть"}</button>
-      </div>
-    `;
-  }
-
-  renderBlock("toast-block", messageText);
-
-  const button = document.getElementById("toast-main-action");
-  if (button != null) {
-    button.onclick = function () {
-      if (action != null && action.handler != null) {
-        action.handler();
-      }
-      renderToast(null, "");
-    };
-  }
+  });
+} else {
+  console.log("This is not browser!");
 }
